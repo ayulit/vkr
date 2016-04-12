ab0c8993394cbff509ea70c9e2ca0e3359f0f5c9
Andrey Litvinov <litwinoff@gmail.com>
2016-04-12 21:46:50 +0300

    code changings according to BigARTM Beta release v0.7.1 winodws x64

--- plsa.py
+++ plsa.py
@@ -16,8 +16,14 @@ import shutil
 topics_num = 3                           # число топиков для кластеризации
 
 collection_name = 'banks'                # название коллекции
-data_folder = 'train/'                   # папка с обучающей выборкой
-target_folder = 'plsa/'+collection_name  # папка с данными BigArtm: батч коллекции и словарь
+data_folder = 'train'                   # папка с обучающей выборкой
+#target_folder = 'plsa/'+collection_name  # папка с данными BigArtm: батч коллекции и словарь
+target_folder = os.path.join('plsa',collection_name)  # папка с данными BigArtm: батч коллекции и словарь
+
+print "target_folder=",target_folder
+
+
+
 csv_folder = 'csv'                       # папка для csv файлов матриц тета и фи
 
 logs_dir = 'logs'                        # папка с логами
@@ -27,11 +33,15 @@ bigartm_logs_dir = 'bigartm'             # BigArtm internal logs specific dir
 # Создание папок
 # ===================================================
 
+
+
+
 # создать папку logs_dir, если её нет
 if not os.path.exists(logs_dir):
     os.makedirs(logs_dir)
 
-bigartm_logs_path = logs_dir + '/' + bigartm_logs_dir
+
+bigartm_logs_path = os.path.join(logs_dir,bigartm_logs_dir)
 if not os.path.exists(bigartm_logs_path):
     os.makedirs(bigartm_logs_path)
 
@@ -44,32 +54,56 @@ shutil.rmtree(csv_folder)
 if not os.path.exists(csv_folder):
     os.makedirs(csv_folder)
 
+
+# Moving BigArtm internal logs to specific dir
+for filename in os.listdir('./'):
+    if os.path.isfile(filename) and filename.startswith('..'):
+        # If you specify the full path to the destination (not just the directory)
+        # then shutil.move will overwrite any existing file
+        shutil.move(os.path.join('./', filename), os.path.join(bigartm_logs_path, filename))
+
+# для надежности принудидельно удалим symlink на лог
+link_name = '..INFO'
+if os.path.islink(link_name):
+    os.unlink(link_name)
+
+
 # ===================================================
 # TODO Конфигурация парсера ОБУЧАЮЩЕЙ коллекции - реализовать в виде функции
 # ===================================================
 
 # идет проверка на наличие какого-то батча в папке
 # TODO скорее всего batch это сохраненная модель или конфиг, позже выясним
-batches_found = len(glob.glob(target_folder + "/*.batch"))
+pathname = os.path.join(target_folder,"*.batch")
+batches_found = len(glob.glob(pathname))
 if batches_found != 0:
     shutil.rmtree(target_folder)
 
 collection_parser_config = artm.messages_pb2.CollectionParserConfig()  # создание объекта конфигуратора
 collection_parser_config.format = artm.library.CollectionParserConfig_Format_BagOfWordsUci  # формат мешка слов
 # укзание на docword и vocab коллекции
-collection_parser_config.docword_file_path = data_folder + 'docword.' + collection_name + '.txt'
-collection_parser_config.vocab_file_path = data_folder + 'vocab.' + collection_name + '.txt'
 
+print "data_folder=",data_folder
+print os.path.join(data_folder,'docword.' + collection_name + '.txt')
+
+collection_parser_config.docword_file_path = os.path.join(data_folder,'docword.' + collection_name + '.txt')
+collection_parser_config.vocab_file_path = os.path.join(data_folder,'vocab.' + collection_name + '.txt')
 collection_parser_config.target_folder = target_folder
 
+print collection_parser_config.target_folder
+
 collection_parser_config.dictionary_file_name = 'dictionary'  # имя файла словаря
+
+print collection_parser_config
 
 # Набор уникальных токенов
 unique_tokens = artm.library.Library().ParseCollection(collection_parser_config)
 
+print unique_tokens
+
 print "\nParsing from TRAIN textual collection: OK.\n"
 
-# sys.exit()  # stop execution
+#sys.exit()  # stop execution
 
 # ===================================================
 # Построение тематической модели
@@ -82,7 +116,9 @@ with artm.library.MasterComponent() as master:
     master.Reconfigure()
 
     # Создание словаря BigArtm c частотами токенов
-    dictionary = master.CreateDictionary(unique_tokens)
+    #dictionary = master.CreateDictionary(unique_tokens)
+
+    master.ImportDictionary('dictionary', os.path.join(target_folder, 'dictionary'))
 
     # print unique_tokens
 
@@ -114,7 +150,8 @@ with artm.library.MasterComponent() as master:
     # Создание модели: задание числа топиков, числа внутренних итераций, применение конфигурации
     model = master.CreateModel(topics_count=topics_num, inner_iterations_count=10, config=config)
     # Инициализация модели
-    model.Initialize(dictionary)  # Setup initial approximation for Phi matrix.
+    #model.Initialize(dictionary)  # Setup initial approximation for Phi matrix.
+    model.Initialize('dictionary')
     # Включение показателей
     model.EnableScore(ru_top_tokens_score)
     model.EnableScore(en_top_tokens_score)
@@ -142,13 +179,15 @@ with artm.library.MasterComponent() as master:
     # Сохранение тета-матрицы (распеределение топиков по документу) ОБУЧАЮЩЕЙ выборки
     # ===================================================
 
-    theta_matrix = master.GetThetaMatrix(model, clean_cache=True)  # извлекаем тета-матрицу из модели
+    theta_matrix, numpy_matrix = master.GetThetaMatrix(model, clean_cache=True)  # извлекаем тета-матрицу из модели
     f = open(csv_folder + '/' + 'theta_train.csv', 'w')            # создаем csv для сохранения
 
-    for item in theta_matrix.item_weights:  # цикл по всем документам тета-матрицы
+
+
+    for item in numpy_matrix:  # цикл по всем документам тета-матрицы
         str1 = ''
         # print item.value
-        for val in item.value:                # цикл по всем топикам документа
+        for val in item:                # цикл по всем топикам документа
           str1 = str1+str(val)+';'              # накапливаем строку, разделенную ";"
         f.write(str1 + '\n')                  # пишем строку в csv файл
 
@@ -194,7 +233,7 @@ with artm.library.MasterComponent() as master:
     test_batch = artm.library.Library().LoadBatch(batches[0])
 
     # Применяем модель к батчу и получаем тета-матрицу тестовой выборки
-    theta_matrix = master.GetThetaMatrix(model=model, batch=test_batch)
+    theta_matrix, numpy_matrix = master.GetThetaMatrix(model=model, batch=test_batch)
 
     # путь к файлу с ручной разметкой тестовой коллекции
     labels_file_path = data_folder + 'labels.' + collection_name + '.txt'
@@ -214,12 +253,17 @@ with artm.library.MasterComponent() as master:
 
     match_counter = 0                                       # счетчик сопадений предсказанной и истинной меток
 
-    for j, item in enumerate(theta_matrix.item_weights):    # цикл по всем документам тета-матрицы
+
+
+
+
+
+    for j, item in enumerate(numpy_matrix):    # цикл по всем документам тета-матрицы
 
         str1 = ''
         docvector=[]                                          # вектор документа тета-матрицы
         #print item.value
-        for val in item.value:                                # цикл по всем топикам документа
+        for val in item:                                # цикл по всем топикам документа
             docvector.append(val)                               # формируем вектор документа
             str1 = str1+str(val)+';'                            # накапливаем строку, разделенную ";"
 
@@ -239,23 +283,18 @@ with artm.library.MasterComponent() as master:
             match_counter += 1                                  # при совпадении меток приращаем счетчик совпадений
         f.write(str1 + '\n')                                    # пишем накопленную строку в файл
 
-    match = match_counter*1.0/len(theta_matrix.item_weights)  # считаем процент совпадений
+    match = match_counter*1.0/len(numpy_matrix)  # считаем процент совпадений
     f.write(str(match)+';' + '\n')                            # и выводим % совпадений в последней строке
     f.close()
 
     shutil.rmtree(target_folder)                              # удаляем все из целевой папки
 
-# Moving BigArtm internal logs to specific dir
-for filename in os.listdir('.'):
-    if os.path.isfile(filename) and filename.startswith('..'):
-        # If you specify the full path to the destination (not just the directory)
-        # then shutil.move will overwrite any existing file
-        shutil.move(os.path.join('.', filename), os.path.join(bigartm_logs_path, filename))
 
-# для надежности принудидельно удалим symlink на лог
-link_name = '..INFO'
-if os.path.islink(link_name):
-    os.unlink(link_name)
+
+
+
+
+
 
 # print 'STOP'
 # sys.exit()  # stop execution
